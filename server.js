2561de67e14baac621c07cd003d52c4eb7e551bd
luke-b <luke.benda@gmail.com>
2018-07-22 18:40:04 +0200

    server path

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+console.log("starting server ... at: " + __dirname);
+
+app.use(express.static(__dirname + '/'));
 
 app.listen(process.env.PORT || 8080);
